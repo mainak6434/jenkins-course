b6f1dbea241dfe5c09d38acbb5bb231affdcac0d
mainak6434 <mainak6434@yahoo.co.in>
2020-06-07 02:50:00 +0530

    Update digitalocean_userdata.sh

--- jenkins-slave/digitalocean_userdata.sh
+++ jenkins-slave/digitalocean_userdata.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-apt-get update
-apt-get install -y docker-engine
+#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+#apt-get update
+#apt-get install -y docker-engine
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+xenial \
+stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
 systemctl enable docker
 mkdir -p /var/jenkins_home/.ssh
 cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
